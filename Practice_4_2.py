b15f88cd8f24f193c8d63834805c89103f442b8d
AliFateminia <48270866+AliFateminia@users.noreply.github.com>
2022-03-24 14:11:26 +0430

    Add files via upload

--- Practice_4_2.py
+++ Practice_4_2.py
@@ -1,8 +1,13 @@
 import math
 def Equation(a, b, c):
-    delta = math.sqrt(abs(b ** 2 - (4 * a * c)))
-    x1 = round(((-b + delta) / (2 * a)), 2)
-    x2 = round(((-b - delta) / (2 * a)), 2)
+    delta = (b ** 2 - (4 * a * c))
+    if delta < 0:
+        print("The equation has no answer in real numbers")
+        Input()
+    else:
+        Sqrt = math.sqrt(delta)
+    x1 = round(((-b + Sqrt) / (2 * a)), 2)
+    x2 = round(((-b - Sqrt) / (2 * a)), 2)
     print("x1: ", x1, "    x2: ", x2)
     Input()
 def Input():
